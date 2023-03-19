82061b79de2532e774f2b0a59c2b9a276f282338
elias <eliasrt2020@gmail.com>
2023-03-19 16:35:12 -0300

    modificacion cors

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const incident_routes = require ("./routes/incident")
+
+app.use(cors());
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
